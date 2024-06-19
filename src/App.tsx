1a2bfbf4b5629cd49aa11b20efab301025394e76
flexxdarker <ksenzhakmax@gmail.com>
2024-06-19 12:38:21 +0300

    robe

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import LoginPage from "./components/login/LoginPage.tsx";
+import CategoriesPage from "./components/categories/CategoriesPage.tsx";
+
+function App() {
+
+    return (
+        <>
+            <CategoriesPage />
+
+
+        </>
+    )
+}
+
+export default App
